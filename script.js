118bc76f3d7956d782f44ab03343891fd596a767
Carina Ullman <carinalullman@gmail.com>
2019-10-21 16:34:48 -0600

    Some progress on the 5 day forecast

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 var APIKey = "a4af8bd3c9453f28d775a17a4db79327";
+//var city = $(this).attr("citysearch");
 //var city = document.getElementById("citysearch").value;
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + "Denver" + "&units=imperial&appid=" + APIKey;
+//can't get the either of the above code to work so I'm just hard coding Denver for the time being
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "Denver" + "&units=imperial&appid=" + APIKey;
 var lat = 51.51;
 var lon = -0.13;
-var queryURLUV = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+var fullDate = new Date()
+var queryURLFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + "Denver" +"&units=imperial&appid=" + APIKey;
 
 document.getElementById("btn").addEventListener("click", query);
 
@@ -28,8 +32,7 @@ function query() {
             $(".temp").text("Temperature (F) " + response.main.temp);
             $(".humidity").text("Humidity: " + response.main.humidity);
             $(".wind").text("Wind Speed: " + response.wind.speed);
-
-
+            $(".card-title").text(fullDate);
 
 
             // Log the data in the console as well
@@ -43,20 +46,35 @@ function query() {
         url: queryURLUV,
         method: "GET"
     })
-        // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
-
-            // Log the queryURL
             console.log(queryURLUV);
-
-            // Log the resulting object
             console.log(response);
-
-            // Transfer content to HTML
             $(".UV").html("UV Index: " + response.value);
 
-            //UV Index$(".")
         });
 
+    $.ajax({
+        url: queryURLFive,
+        method: "GET"
+    })
 
+        .then(function (response) {
+            console.log(queryURLFive);
+            console.log(response);
+            //this could use a for loop to get the array of temps
+
+            $("#temp1").text("Temperature (F) " + response.list[0].main.temp);
+            $("#hum1").text("Humidity: " + response.list[0].main.humidity);
+
+            console.log("Temperature: " + response.list[0].main.temp);
+            console.log("Humidity: " + response.list[0].main.humidity);
+
+        });
 }
+
+
+
+
+
+
+
