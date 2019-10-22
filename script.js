298f2c6546765e4dd9034228dcaef73e5e9753da
Carina Ullman <carinalullman@gmail.com>
2019-10-21 22:28:12 -0600

    Final but could use some color

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ console.log(n);
 document.getElementById("btn").addEventListener("click", query);
 
 function query() {
-
+document.getElementById("weatherresponse").style.display="";
     var city = document.getElementById("citysearch").value;
     var queryURLLatLng = "https://api.opencagedata.com/geocode/v1/json?q=" + city + "&key=" + APIKeyOpenCage;
     $.ajax({
@@ -75,9 +75,6 @@ function query() {
             $("#day").text(n);
             $("h3").text("5-Day Forecast for " + city);
 
-
-
-
             // Log the data in the console as well
             console.log("Wind Speed: " + response.wind.speed);
             console.log("Humidity: " + response.main.humidity);
@@ -101,19 +98,24 @@ function query() {
             $(".card-title4").text(weekday[(d.getDay() + 4) % 7]);
             $(".card-title5").text(weekday[(d.getDay() + 5) % 7]);
 
-
+            //$("#icon1").attr("src", response.list[0].weather[0].icon + ".png");
+            $("#icon1").attr("src", "partly cloudy.png");
             $("#temp1").text("Temperature (F): " + response.list[0].main.temp);
             $("#hum1").text("Humidity: " + response.list[0].main.humidity);
 
+            $("#icon2").attr("src", "partly cloudy.png");
             $("#temp2").text("Temperature (F): " + response.list[1].main.temp);
             $("#hum2").text("Humidity: " + response.list[1].main.humidity);
 
+            $("#icon3").attr("src", "partly cloudy.png");
             $("#temp3").text("Temperature (F): " + response.list[2].main.temp);
             $("#hum3").text("Humidity: " + response.list[2].main.humidity);
 
+            $("#icon4").attr("src", "partly cloudy.png");
             $("#temp4").text("Temperature (F): " + response.list[3].main.temp);
             $("#hum4").text("Humidity: " + response.list[3].main.humidity);
 
+            $("#icon5").attr("src", "partly cloudy.png");
             $("#temp5").text("Temperature (F): " + response.list[4].main.temp);
             $("#hum5").text("Humidity: " + response.list[4].main.humidity);
 
